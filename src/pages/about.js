6fb2b68273b9e9c86f267342adbbf6d9c5b5507f
Bettenoch <bettenoch254@gmail.com>
2024-02-01 09:42:57 +0300

    Change education Image.

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import AnimatedText from "@/components/AnimatedText";
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import React, { useEffect, useRef } from "react";
-import profilePic from "../../public/images/profile/developer-pic-2.jpg";
+import profileImage from "../../public/images/profile/eduimage.PNG";
 import Image from "next/image";
 
 import { useInView, useMotionValue, useSpring } from "framer-motion";
@@ -48,7 +48,7 @@ const about = () => {
                 Biograpy
               </h2>
               <p className="font-medium">
-                - Hi, Im CodeBucks, a web developer and UI/UX designer with a
+                - Hi, Im Bett Enock, a web developer and UI/UX designer with a
                 passion for creating beautiful, functional, and user-centered
                 digital experiences. With 4 years of experience in the field. I
                 am always looking for new and innovative ways to bring my
@@ -70,7 +70,7 @@ const about = () => {
             <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light dark:bg-dark dark:border-light p-8 xl:col-span-4 md:order-1 md:col-span-8">
               <div className="absolute top-0 -right-3 -z-10 w-[102%]  h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
               <Image
-                src={profilePic}
+                src={profileImage}
                 alt="BKE"
                 className="w-full h-auto rounded-2xl"
                 priority
