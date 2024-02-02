a5f8b15c8cb96f76cc5e2a0448a5941e7fd89e33
Bettenoch <bettenoch254@gmail.com>
2024-02-02 09:56:32 +0300

    Refactor page design.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -16,10 +16,10 @@ const Info = ({position, company, companyLink, time, address, work}) => {
                     {position}&nbsp; 
                     <a href={companyLink} target='_blank' className='text-primary dark:text-primaryDark capitalize'>@{company}</a> 
                 </h3>
-                <span className='capitalize font-medium text-medium text-dark/75 dark:text-light/75 xs:text-sm'>
+                <span className='capitalize font-medium text-medium pb-4 text-dark/75 dark:text-light/75 xs:text-sm'>
                     {time} | {address}
                 </span>
-                <p className='font-medium w-full md:text-sm'>
+                <p className='font-medium w-full mt-2 md:text-sm'>
                     {work}
                 </p>
             </motion.div>
@@ -51,47 +51,37 @@ const Experience = () => {
             </motion.div>
             <ul className='w-full flex flex-col items-center justify-between ml-4 xs:ml-2'>
                <Info
-                position= "Software Engineer"
-                company="Google"
-                companyLink="www.google.com"
-                time="2022-Present"
-                address= "Mountain View, CA"
-                work="Worked on a team responsible for developing new features for Google's 
-                search engine, including improving the accuracy and relevance of search results and 
-                developing new tools for data analysis and visualization." 
+                position= "Full-stack web developer & Mentor"
+                company="MICROVERSE"
+                companyLink="https://www.microverse.org/about-us"
+                time="2023 February-Present"
+                address= "San Francisco, California "
+                work="Acquired expertise in intermediate and advanced aspects of full-stack development, encompassing Rails, React Git, and data structures & algorithms challenges. 
+                Successfully executed multiple full-stack projects through both independent work and collaborative pair-programming efforts.
+                " 
                 
                 />
                  <Info
-                position= "Software Engineer"
-                company="Google"
-                companyLink="www.google.com"
-                time="2022-Present"
+                position= "GIS Analyst Intern"
+                company="BOMET-COUNTY GIS & MAPPING"
+                companyLink="https://www.microverse.org/about-us"
+                time="2021 June-2021 September"
                 address= "Mountain View, CA"
-                work="Worked on a team responsible for developing new features for Google's 
-                search engine, including improving the accuracy and relevance of search results and 
-                developing new tools for data analysis and visualization." 
+                work="Assisted the GIS department in migrating from paper-work cadastral forms to digital formats. This enabled the department to install modern GIS software such as QGIS, ArcGISARCGIS & MATPLOT and serve more than 50 clients daily.
+                Participated in the digital mapping and analysis of the planned expansion of Sotik Town, which won the award as the best priority county project plan.
+                Mentored junior interns from other colleges to adopt modern GIS and Mapping frameworks.
+                Coordinated 8 Interns during fieldwork mapping and measurement sessions.
+                " 
                 
                 />
                  <Info
-                position= "Software Engineer"
-                company="Google"
-                companyLink="www.google.com"
-                time="2022-Present"
+                position= "Survey Assistant"
+                company="Self-Employed"
+                companyLink="https://www.microverse.org/about-us"
+                time="2022 June-2022 November"
                 address= "Mountain View, CA"
-                work="Worked on a team responsible for developing new features for Google's 
-                search engine, including improving the accuracy and relevance of search results and 
-                developing new tools for data analysis and visualization." 
-                
-                />
-                 <Info
-                position= "Software Engineer"
-                company="Google"
-                companyLink="www.google.com"
-                time="2022-Present"
-                address= "Mountain View, CA"
-                work="Worked on a team responsible for developing new features for Google's 
-                search engine, including improving the accuracy and relevance of search results and 
-                developing new tools for data analysis and visualization." 
+                work="Worked on a team responsible for conducting surveys using RTK and drone mapping around Bomet.
+                Participated in solving boundary disputes and conducting safe searches for parcels before procurement for clients" 
                 
                 />
             </ul>
