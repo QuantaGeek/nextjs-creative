331aa51ed22493faf696d4baf717af02b5bdaeeb
Bettenoch <bettenoch254@gmail.com>
2024-02-02 13:18:39 +0300

    use nav component.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Footer from '@/components/Footer'
-import Navbar from '@/components/Navbar'
+// import Navbar from '@/components/Navbar'
+import Navigation from '@/components/Navigation'
 import '@/styles/globals.css'
 import {Montserrat} from 'next/font/google'
 import Head from 'next/head'
@@ -18,7 +19,7 @@ export default function App({ Component, pageProps }) {
           <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={ `${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
-          <Navbar></Navbar>
+          <Navigation />
           <Component {...pageProps} />
           <Footer />
       </main>
